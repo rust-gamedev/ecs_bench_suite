ab54d5515810d2c2d402ae4410b66b953c6f07ba
jojolepro <jojolepro@jojolepro.com>
2021-03-09 13:50:12 -0500

    run benchmark.

--- src/planck_ecs/add_remove.rs
+++ src/planck_ecs/add_remove.rs
@@ -23,7 +23,7 @@ impl Benchmark {
     pub fn run(&mut self) {
         let b_storage = &mut self.2;
         for entity in &self.0 {
-            b_storage.insert(*entity, B(0.0)).unwrap();
+            b_storage.insert(*entity, B(0.0));
         }
 
         for entity in &self.0 {
